eecf2f64f53b8e90570efc8a47d6ddb9b43bb12d
Dhruv Pai Dukle <dhruv@Dhruvs-MacBook-Pro.local>
2023-11-16 12:59:46 +0530

    1

---
+++ authtoken.py
@@ -0,0 +1,2 @@
+auth_token = "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"
+# How to get one: https://huggingface.co/docs/hub/security-tokens
---
+++ main.py
@@ -0,0 +1,114 @@
+import streamlit as st
+import cohere as c
+import os
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+st.title('AI Based board Game Generator')
+
+
+#Inputs for game concept
+st.text("Game concept")
+
+#Game theme input
+theme = st.text_input("What theme or setting do you want for your board game?")
+
+#Game Genre input
+genre = st.text_input("What genre would you like your game to be in like would it be a strategy game like chess, or a card playing game etc?")
+
+st.text("Game Mechanics")
+
+winning_conditions = st.text_input("What are the winning conditions for the game?")
+
+st.text('That will be it! that is all I need from you')
+
+
+co = c.Client('r2Jf4drl1pblR5kzVJAjyEHggaAlKQZSmR49T3tC')
+
+message = "Hello Mr etuite I heard you make pretty nice custom board games. could you make one for me I have everything you need to get started!I already have a manufacturing team ready so can you be very concise on the details of the board game and be a bit on the longer side please be good and detailed layout the custom game mechanics THAT YOU MR ETUITE HAS TO DESIGN .also output a list of props to use in the game .also please do a mock round of playing the game a full round like with 10 rounds of the game itself also design a game manual of sorts that comes with board games you buy.You will be given a hefty reward if the game works properly and fits my interests! I alse need you to come up with an AI based image generator prompt I WANT YOU TO GENERATE ONE FOR THE BOARD AND MORE FOR THE PROPS IT SHOULD BE IN THE FOLLOWING FORMAT PLEASE FOLLOW THAT FORMAT YOU HAVE TO LIKE GENERATE THE PROMPT IN THAT FORMAT!:Here's a short and sweet prompt for an AI image generator:FOLLOWED BY THE ACTUAL PROMPT" + theme + genre + winning_conditions
+import cohere
+import uuid
+
+co = cohere.Client("RoX9tV4GM1PpPKZpTJGy04JCZ35KkzPwpAxo9XAU")
+
+if(winning_conditions):
+    
+    conversation_id = str(uuid.uuid4()) # Generate a new conversation_id
+    response = co.chat(message=message,
+    preamble_override="You are an expert board game designer",
+    conversation_id=conversation_id)
+    
+    st.write(response.text)
+    
+    separator = "Prompt"
+
+# Split the response using the separator
+    parts = response.text.split(separator)
+
+# Take the part before the separator
+    board_game_description = parts[0]
+    
+
+    separator2 = "Props:"
+
+# Split the response using the separator
+    parts = response.text.split(separator)
+
+# Take the part before the separator
+    board_game_props = parts[0]
+    
+# Our Host URL should not be prepended with "https" nor should it have a trailing slash.
+    os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+# Sign up for an account at the following link to get an API Key.
+# https://platform.stability.ai/
+
+# Click on the following link once you have created an account to be taken to your API Key.
+# https://platform.stability.ai/account/keys
+
+# Paste your API Key below.
+
+    os.environ['STABILITY_KEY'] = 'sk-ZIcCMyOZjaDgn3lKIWRBUaVdAkCJhtL8YIV2zxpnjPPv052b'
+
+# Set up our connection to the API.
+    stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-xl-1024-v1-0", # Set the engine to use for generation.
+    # Check out the following link for a list of available engines: https://platform.stability.ai/docs/features/api-parameters#engine
+)
+
+# Set up our initial generation parameters.
+    answers = stability_api.generate(
+        prompt=board_game_description,
+        seed=4253978046, # If a seed is provided, the resulting generated image will be deterministic.
+                     # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                     # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+        steps=50, # Amount of inference steps performed on image generation. Defaults to 30. 
+        cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
+                   # Setting this value higher increases the strength in which it tries to match your prompt.
+                   # Defaults to 7.0 if not specified.
+        width=1024, # Generation width, defaults to 512 if not included.
+        height=1024, # Generation height, defaults to 512 if not included.
+        samples=1, # Number of images to generate, defaults to 1 if not included.
+        sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
+                                                 # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
+                                                 # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m, k_dpmpp_sde)
+            )
+
+# Set up our warning to print to the console if the adult content classifier is tripped.
+# If adult content classifier is not tripped, save generated images.
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save("image.png") # Save our generated images with their seed number as the filename.
+
+      
+                st.image("image.png")
